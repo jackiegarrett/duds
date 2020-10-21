844df8da4422f08a42b98151567b5b5561c3d890
austin <austinkandt@gmail.com>
2020-10-21 10:53:16 -0400

    Genres dictionary added to watchlist and date year pipe

--- src/app/watchlist-page/watchlist-page.component.ts
+++ src/app/watchlist-page/watchlist-page.component.ts
@@ -15,8 +15,31 @@ export class WatchlistPageComponent implements OnInit {
   posterPath: string = 'https://image.tmdb.org/t/p/w154';
   genresArray: any[] = [];
   movieInfo;
+  genresDictionary;
 
   ngOnInit(): void {
+    this.genresDictionary = {
+      '28': 'Action',
+      '12': 'Adventure',
+      '16': 'Animation',
+      '35': 'Comedy',
+      '80': 'Crime',
+      '99': 'Documentary',
+      '18': 'Drama',
+      '10751': 'Family',
+      '14': 'Fantasy',
+      '36': 'History',
+      '27': 'Horror',
+      '10402': 'Music',
+      '9648': 'Mystery',
+      '10749': 'Romance',
+      '878': 'Science Fiction',
+      '10770': 'TV Movie',
+      '53': 'Thriller',
+      '10752': 'War',
+      '37': 'Western',
+    };
+
     this.getApiService.getGenres().subscribe((result: any) => {
       console.log('result', result);
       this.genresArray = result.genres;
